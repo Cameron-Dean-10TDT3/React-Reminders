d288ebadd0f98158825628814c4e6d17f185d917
Cameron-Dean-10TDT3 <c.c.dean@student.saintkentigern.com>
2020-03-19 16:33:42 +1300

    4 files modified, 1 added

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import './style.css';
-import logo from 'images/logo.png'
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./style.css";
+import logo from "./images/logo.png"
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'Cameron'
+      name: "Cameron"
     };
   }
-
+  
   render() {
     return (
       <div>
         <h1>React Reminders</h1>
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-        <img src={logo}></img>
+        <p>Start editing to see some magic happen :)</p>
+        <img src={logo}/>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
