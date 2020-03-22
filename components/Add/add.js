aa2d7125ecce1a7b6f55eb430e718bebc2d59afe
Cameron-Dean-10TDT3 <c.c.dean@student.saintkentigern.com>
2020-03-23 12:03:42 +1300

    1 file modified, 2 added

---
+++ components/Add/add.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "../../style.css";
+import { NavLink } from "react-router-dom";
+import Helmet from "react-helmet";
+
+const TITLE = 'Add a Reminder'
+
+const Add = () => {
+  return (
+    <div className="addScreen">
+      <Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
+      <h3>Reminder:</h3>
+      <form>
+        <label>
+          <input type="text" />
+        </label>
+        <br />
+        <br />
+        Reminder Date:
+        <br />
+        <input type="date" />
+        <br />
+        <br />
+        Reminder Time:
+        <br />
+        <input type="time" />
+        <div className="btnPosition" id="btnAdd">
+          <NavLink to="/List">
+            <button className="btn">Add Reminder</button>
+          </NavLink>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Add;
