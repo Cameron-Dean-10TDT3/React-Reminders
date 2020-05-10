dfc3f2a95583189d653bbf67628ee7e3a23d7ce6
Cameron-Dean-10TDT3 <c.c.dean@student.saintkentigern.com>
2020-05-11 10:39:25 +1200

    1 file modified

--- components/Error/index.js
+++ components/Error/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 
-const TITLE = 'Error'
-
 const Error = () => {
   return (
     <div>
-      <Helmet>
-        <title>{TITLE}</title>
-      </Helmet>
-      // Fallback page if url is entered incorrectly
+      {/* Fallback page if url is entered incorrectly */}
       <h1>Error: Page does not exist!</h1>
     </div>
   );
